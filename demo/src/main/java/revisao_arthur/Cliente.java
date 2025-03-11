17c062e84f1e49686f8926461b66cdec69b592fc
ARTHUR WINTER ZIMMERMANN <arthur.zimmermann@univille.br>
2025-03-10 22:05:32 -0300

    feat: revisao

---
+++ demo/src/main/java/revisao_arthur/Cliente.java
@@ -0,0 +1,16 @@
+package revisao_arthur;
+
+public class Cliente {
+    //variável - atributo
+    private String nome;
+
+    //método
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
---
+++ demo/src/main/java/revisao_arthur/Main.java
@@ -0,0 +1,11 @@
+package revisao_arthur;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        Cliente zezinho = new Cliente();
+        zezinho.setNome("zezinho");
+        Cliente mariazinha = new Cliente();
+        mariazinha.setNome("mariazinha");
+    }
+}
