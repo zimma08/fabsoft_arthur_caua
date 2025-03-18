b6588dd4e4b32c621ca70b098d94707ec679b8af
CAUÃ RICHLIN <caua.richlin@univille.br>
2025-03-17 22:00:14 -0300

    feat: novos metodos

--- demo/src/main/java/revisao_arthur/Cliente.java
+++ demo/src/main/java/revisao_arthur/Cliente.java
@@ -4,6 +4,22 @@ public class Cliente {
     //variável - atributo
     private String nome;
 
+    //construtor - mesmo nome da classe e não te retorno
+    //inicializar atributos
+    //obrigar a passagem de valores 
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    //polimorfismo - várias formas de existir a mesma coisa
+    public Cliente() {
+        
+    }
+    //método
+    @Override
+    public String toString(){
+        return getNome();
+    }
+
     //método
     public String getNome() {
         return nome;
@@ -12,5 +28,6 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    //encapsulamento 
 
 }
--- demo/src/main/java/revisao_arthur/Main.java
+++ demo/src/main/java/revisao_arthur/Main.java
@@ -3,9 +3,15 @@ package revisao_arthur;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-        Cliente zezinho = new Cliente();
-        zezinho.setNome("zezinho");
-        Cliente mariazinha = new Cliente();
-        mariazinha.setNome("mariazinha");
+
+        Cliente zezinho = new Cliente("Zezinho");
+        zezinho.setNome("zezinho da silva Sauro");
+        Cliente mariazinha = new Cliente("Mariazinha");
+        mariazinha.setNome("Mariazinha");
+
+
+        System.out.println(zezinho);
+        System.out.println(mariazinha);
+
     }
 }
