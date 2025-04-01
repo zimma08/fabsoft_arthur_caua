33da9c8b44ac4fa447afa96987d637b7befd39d4
ARTHUR WINTER ZIMMERMANN <arthur.zimmermann@univille.br>
2025-03-31 22:09:40 -0300

    controller

--- demo/src/main/java/revisao_arthur/view/FrmMain.java
+++ demo/src/main/java/revisao_arthur/view/FrmMain.java
@@ -1,20 +1,67 @@
 package revisao_arthur.view;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class FrmMain extends JFrame{
+
+public class FrmMain extends JFrame {
+    private JLabel lblId, lblNome, lblEndereco;
+    private JTextField txtId, txtNome, txtEndereco;
+
+    private JButton btnSalvar, btnSair;
+
 
     public FrmMain() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(500,500);
         setTitle("Meu primeiro Swing");
-        var texto =  new JLabel("Meu texto");
-        getContentPane().add(texto);
+        //var texto = new JLabel("Meu texto");
+        //getContentPane().add(texto);
+        painelCampos();
         setVisible(true);
-        
-
     }
+    private void painelBotoes(){
+        ControllerCliente controller = new ControllerCliente();
+        JPanel painel = new JPanel();
+        btnSalvar= new JButton("Salvar");
+        painel.add(btnSalvar);
+        btnSalvar.addActionListener(controller);
+
+        btnSair = new JButton("Sair");
+        painel.add(btnSair);
+        btnSair.addActionListener(controller);
+
+        getContentPane().add(painel,"South");
+    }
+
+    private void painelCampos(){
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(3,2));
+        lblId = new JLabel("ID");
+        painel.add(lblId);
+        txtId = new JTextField(5);
+        painel.add(txtId);
+
+        lblNome = new JLabel("Nome");
+        painel.add(lblNome);
+        txtNome = new JTextField(15);
+        painel.add(txtNome);
+
+        lblEndereco = new JLabel("Endereço");
+        painel.add(lblEndereco);
+        txtEndereco = new JTextField(15);
+        painel.add(txtEndereco);
+
+        getContentPane().add(painel,"North");
+    }
+
+
+
     public static void main(String[] args) {
         new FrmMain();
     }
---
+++ demo/target/classes/revisao_arthur/controller/ControllerCliente.java
@@ -0,0 +1,15 @@
+package br.univille.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class ControllerCliente implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "EU NAO ACREDITO");
+    }
+
+}
