d4e277539190b8d7125d95e7ec2d68a436fd1c17
CAUÃ RICHLIN <caua.richlin@univille.br>
2025-04-07 22:06:28 -0300

    Diagrama

---
+++ projfabsoftcarros/src/main/java/br/univille/projfabsoftcarros/entity/Order.java
@@ -0,0 +1,43 @@
+package br.univille.projfabsoftcarros.entity;
+
+import java.time.LocalDate;
+
+public class Order {
+    private Long id;
+    private LocalDate date;
+    private Customer customer;
+    private Car car;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
