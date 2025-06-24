a6a047c4c558d1293356bc4d78da36a1bab0d9e4
zimma08 <arthur.zimmermann@univille.br>
2025-06-24 00:54:41 +0000

    correção

--- projfabsoft-fronted/src/app/app.routes.ts
+++ projfabsoft-fronted/src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { FormClienteComponent } from './form-cliente/form-cliente.component';
 export const routes: Routes = [
     {path: 'clientes', component: ClienteComponent},
     {path: 'clientes/novo', component: FormClienteComponent},
-    {path: 'clientes/alterar/id', component: FormClienteComponent}
+    {path: 'clientes/alterar/:id', component: FormClienteComponent}
 ];
--- projfabsoft-fronted/src/app/cliente/cliente.component.ts
+++ projfabsoft-fronted/src/app/cliente/cliente.component.ts
@@ -3,13 +3,17 @@ import { Cliente } from '../model/cliente';
 import { ClienteService } from '../service/cliente.service';
 import { Router } from '@angular/router';
 import * as bootstrap from 'bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cliente',
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css',
-  providers: [ClienteService]
+  providers: [ClienteService, Router]
 })
+
 export class ClienteComponent implements OnInit {
   listaClientes: Cliente[] = [];
 
@@ -35,14 +39,14 @@ export class ClienteComponent implements OnInit {
   }
 
   novo() {
-    this.router.navigate(['cliente/novo']);
+    this.router.navigate(['clientes/novo']);
   }
 
-  alterar(cliente: Cliente) {
+  alterar(cliente:Cliente) {
     this.router.navigate(['clientes/alterar', cliente.id]);
   }
 
-  abrirConfirmacao(cliente: Cliente) {
+  abrirConfirmacao(cliente:Cliente) {
     this.clienteSelecionado = cliente;
     this.modal = new bootstrap.Modal(this.modalElement.nativeElement);
     this.modal.show();
